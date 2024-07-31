d83acdd8011f015e8240c3bf7b6894af67a958bb
Sourabh Paralikar <paralikarsourabh007@gmail.com>
2024-07-31 12:49:26 +0530

    ttt initialise

---
+++ src/main/java/task/time/tracker/TaskTimeTrackerApplication.java
@@ -0,0 +1,13 @@
+package task.time.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTimeTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTimeTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/task_time_tracker/TaskTimeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package task_time_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTimeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
